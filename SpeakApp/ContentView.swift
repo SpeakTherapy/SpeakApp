e4a0093ef70a556ebc5a4ff77b0d2f3f8659a244
tannyb28 <tanishbhowmick@gmail.com>
2024-05-22 20:01:12 -0500

    Initial Commit

---
+++ SpeakApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeakApp
+//
+//  Created by Tanish Bhowmick on 5/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeakApp/SpeakAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakAppApp.swift
+//  SpeakApp
+//
+//  Created by Tanish Bhowmick on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SpeakAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
