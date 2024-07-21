38142a299ca863bf6a951509e503e9069410ca85
Tanish Bhowmick <tanishbhowmick@gmail.com>
2024-07-21 16:23:02 -0500

    Initial Commit

---
+++ PeakSpeak/PeakSpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeakSpeakApp.swift
+//  PeakSpeak
+//
+//  Created by Tanish Bhowmick on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PeakSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
